403936dfa8622cfa299befca23fb81106832bb27
GuilhermeTapia <guilhermeenrique00@gmail.com>
2024-09-11 13:57:38 +0000

    String

---
+++ aula.js
@@ -0,0 +1,11 @@
+/*console.log('Hello' + ' ' + 'World')
+
+console.log('Hello' + 'World')
+
+myName = "Guilherme"
+console.log("Hello" + ' ' + myName)
+
+myName = "Guilherme"
+console.log(`Hello, ${myName}`)*/
+
+console.log(15 > 10)
