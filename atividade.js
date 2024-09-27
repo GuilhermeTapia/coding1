f26627b4914cf6e6428e663f37041c9fff5c9c8a
GuilhermeTapia <guilhermeenrique00@gmail.com>
2024-09-27 04:13:40 +0000

    Minha atividade

---
+++ atividade.js
@@ -0,0 +1,73 @@
+myName = "Guilherme"
+myAge = "19"
+console.log(`Olá, meu nome é ${myName} e tenho ${myAge} anos.`);
+
+myName = "Guilherme"
+myCity = "Recife"
+console.log(`${myName} é de ${myCity}`);
+
+const a = 10
+const b = 20
+const soma = (a+b)
+console.log(soma);
+
+const c = 10
+const d = 20
+const subtrair = (a-b)
+console.log(subtrair);
+
+const e = 10
+const f = 20
+const multiplicar = (a*b)
+console.log(multiplicar);
+
+const g = 10
+const h = 20
+const dividir = (a/b)
+console.log(dividir);
+
+const base = 30
+const altura = 20
+const area = (base*altura/2)
+console.log(`a área do triângulo é ${area}`);
+
+const media1 = 7
+const media2 = 5
+const media3 = 10
+const mediatotal = (media1+media2+media3)
+console.log(`O resultado da média é ${mediatotal}`);
+
+const preço = 200
+const desconto = 30
+const valorFinal = (preço*desconto/100)
+console.log(`O preço com desconto é ${valorFinal}`);
+
+const valorBruto = 3000
+const IR = 15
+const INSS = 12
+const valor1 = (valorBruto*12/100)
+const valor2 = (valorBruto-valor1)
+const dedução = (valor2*15/100)
+console.log(`O preço da dedução é ${dedução}R$`);
+
+const dólar1 = 1
+const real1 = 5.44
+const conversão1 = (dólar1*real1)
+console.log(`O valor de 1 dólar em reais é de ${conversão1}`)
+const real2 = 1
+const dólar2 = 5.44
+const conversão2 = (real2/dólar2)
+console.log(`O valor de 1 real em dólares é de ${conversão2}`);
+
+const celsius = 30
+const fahrenheit = ((celsius*9/5)+32)
+console.log(`O valor de celsius para fahrenheit é de ${fahrenheit}°F`);
+
+const peso = 70
+const Altura = 1.78
+const IMC = (peso/(Altura*Altura))
+console.log(`O valor do IMC é ${IMC}`)
+
+
+
+
--- aula.js
+++ aula.js
@@ -9,3 +9,15 @@ myName = "Guilherme"
 console.log(`Hello, ${myName}`)
 
 console.log(15 > 10)
+
+var prompt = require('prompt-sync')();
+
+let nome = prompt("Qual o seu nome?")
+let turma = prompt("Qual a sua turma?")
+let idade = prompt("Qual a sua idade?")
+let horario = prompt("Qual o seu horário de aula?")
+
+console.log(`Bem vindo a faculdade Senac, ${nome}!`);
+console.log(`Sua turma é ${turma}.`);
+console.log(`Você tem ${idade} anos.`);
+console.log(`Sua aula inicia às ${horario} horas.`);
